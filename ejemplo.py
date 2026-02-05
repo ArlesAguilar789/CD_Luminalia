1362bf1d297c4ede8bbd6f1385fc59c404c9f556
ArlesAguilar789 <arlesaguilar300805@gmail.com>
2026-02-05 16:12:26 -0600

    Actualizo proyecto

--- ejemplo.py
+++ ejemplo.py
@@ -1,1 +1,6 @@
 import cv2 as cv
+
+img = cv.imread("C:\\Users\\Arles Aguilar\\Downloads\\11.png")
+cv.imshow('img',img)
+cv.waitKey()
+cv.destroyAllWindows()
---
+++ versiones.py
@@ -0,0 +1,6 @@
+import sys
+print("PYTHON:", sys.executable)
+print("VERSION:", sys.version)
+
+import cv2
+print("OpenCV OK:", cv2.__version__)
